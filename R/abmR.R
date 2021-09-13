b9c39cca5d5c4d4531e11a519fa3832b0800a84b
Ben Gochanour <42599357+bgoch5@users.noreply.github.com>
2021-09-12 20:27:35 -0500

    updates

---
+++ R/abmR.R
@@ -0,0 +1,12 @@
+#' Agent based modeling package
+#'
+#' A work-in-progress package for running agent based models in R.
+#'
+#' @docType package
+#'
+#' @author Benjamin Gochanour \email{ben.gochanour@gmail.com}
+#' @author Javier Fernandez-Lopez \email{jflopez.bio@gmail.com}
+#' @author Andrea Contina \email{acontina@usgs.gov; acontina@gmail.com}
+#'
+#' @name abmR
+NULL
